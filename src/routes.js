094565c8de2da14cc0c830315e747d8a67c0663d
Danilo Losi <danilo.lsanctis@hotmail.com>
2020-07-06 16:29:31 -0300

    validando dados do User

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-    const user = await User.create({
-        name: 'Danilo Losi',
-        email: 'danilo.lsanctis@hotmail.com',
-        password_hash: '65d465sa4d',
-    });
-
-    return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
