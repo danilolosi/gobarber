28c856de826855d674816d9e7a47dadaa2b089fc
Danilo Losi <danilo.lsanctis@hotmail.com>
2020-07-06 10:51:28 -0300

    inserindo User na base

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World!' });
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Danilo Losi',
+        email: 'danilo.lsanctis@hotmail.com',
+        password_hash: '65d465sa4d',
+    });
+
+    return res.json(user);
 });
 
 export default routes;
