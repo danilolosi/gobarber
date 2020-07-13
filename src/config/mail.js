3f26fc822057ef89c8cf10f4c0debce9a563be16
Danilo Losi <danilo.lsanctis@hotmail.com>
2020-07-13 10:32:27 -0300

    add nodemailer and cancel appointment

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: 'ff76ca41755dac',
+        pass: 'ff15b58605b24b',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>',
+    },
+};
